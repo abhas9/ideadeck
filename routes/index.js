dc51c017e173096b88a618d9e6538df7dd02aaf8
Abhas Tandon <abhastandon007@gmail.com>
2016-09-04 00:19:21 +0530

    Change content

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Donation */
 router.get('/donate/:id', function(req, res, next) {
   /* Decide redirect based on stored configuration */
   res.redirect(
     'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=foo@gmail.com&item_name=Support+IdeaDeck'
+  );
+});
+
+/* Buy now */
+router.get('/buy/:id', function(req, res, next) {
+  /* Decide redirect based on stored configuration */
+  res.redirect(
+    'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=foo@gmail.com&item_name=Support+IdeaDeck&amount=5&currency_code=USD'
   );
 });
 
