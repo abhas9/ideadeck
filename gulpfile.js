2e1d0c24d4c19fbd4d2702d8c5b63177bd2f255f
Abhas Tandon <abhastandon007@gmail.com>
2016-09-26 16:29:07 +0530

    Include build script

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var gs = require('gulp-selectors');
+var minify = require('gulp-minifier');
+var imagemin = require('gulp-imagemin');
+var dest = './public';
+
+gulp.task('buildIndex', function() {
+  gulp.src(['./src/**/styles.css', './src/**/index.html'])
+    .pipe(gs.run({
+      'css': ['css'],
+      'html': ['html']
+    }, {
+      ids: '*', // ignore all IDs,
+      classes: ['c_*']
+    }))
+    .pipe(minify({
+      minify: true,
+      collapseWhitespace: true,
+      conservativeCollapse: true,
+      minifyJS: true,
+      minifyCSS: true,
+      getKeptComment: function(content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+      }
+    }))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('minifyCSS', function() {
+  gulp.src(['./src/**/form.css', './src/**/gallery.css',
+      './src/**/carousel.css', './src/**/success.css'
+    ])
+    .pipe(minify({
+      minify: true,
+      collapseWhitespace: true,
+      conservativeCollapse: true,
+      minifyJS: true,
+      minifyCSS: true,
+      getKeptComment: function(content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+      }
+    }))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('images', function() {
+  var imgSrc = './src/**/*.+(jpg|png|svg)',
+    imgDst = dest;
+  return gulp.src(imgSrc)
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+
+gulp.task('default', ['images', 'buildIndex', 'minifyCSS']);
