0f54ef01617af7c87addc308b23cd1295808c94d
Abhas Tandon <abhastandon007@gmail.com>
2016-09-26 16:58:20 +0530

    Improve IdeaDeck landing page

--- routes/index.js
+++ routes/index.js
@@ -216,25 +216,17 @@ router.get('/landing/:path/edit', function(req, res) {
 });
 
 /* Donation */
-router.get('/donate/:id', function(req, res, next) {
+router.get('/donate/', function(req, res, next) {
   /* Decide redirect based on stored configuration */
   res.redirect(
-    'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=foo@gmail.com&item_name=Support+IdeaDeck'
-  );
-});
-
-/* Buy now */
-router.get('/buy/:id', function(req, res, next) {
-  /* Decide redirect based on stored configuration */
-  res.redirect(
-    'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=foo@gmail.com&item_name=Support+IdeaDeck&amount=5&currency_code=USD'
+    'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=abhastandon007@gmail.com&item_name=Support+IdeaDeck'
   );
 });
 
 /* POST subscribe form */
-router.post('/subscribe/:id', function(req, res, next) {
+router.post('/subscribe/:id?', function(req, res, next) {
   /* Decide redirect based on stored configuration */
-  var id = req.params.id;
+  var id = (req.params.id) ? req.params.id : 'abhastandon007@gmail.com';
   res.redirect(307,
     'http://formspree.io/' + id
   );
